7b38a765a270317239bf4ea7b4079ed322047858
unknown <vengammapacha@gmail.com>
2022-01-28 18:36:30 +0530

    done

---
+++ Devops_GradedAssignment/src/main/java/com/DevopsGradedAssignmentApplication.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsGradedAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsGradedAssignmentApplication.class, args);
+		System.err.println("Hello Dev-Ops");
+	}
+
+}
